f87162b12fd96f181fae5423f6aff9d4aee77f4d
Muhammad Tayyab Bin Saleem <ichbintayyab@gmail.com>
2025-08-01 16:39:55 +0500

    Add files via upload

---
+++ 01. Introduction/05. Program to demonstrate use of main() function.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// Execution of any C++ program starts from main()
+int main(){
+	cout<<"This is executed inside the main() function."<<endl;
+	return 0;
+}
